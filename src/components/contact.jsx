3a10f377b1ea448f5ec1088303d005b183071c8c
Chitranjan Gupta <37315561+chitranjan-gupta@users.noreply.github.com>
2024-09-01 18:23:15 +0530

    Update contact.jsx
    Fixing netlify contact form

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -161,7 +161,7 @@ const Contact = () => {
                     </label>
                     <input
                       type="text"
-                      name="first-name"
+                      name="name"
                       placeholder="John"
                       className="block w-full px-5 py-2.5 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-lg dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40"
                     />
